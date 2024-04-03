1edca6d8e0a22b43b42c2b1dbe936ce932a999e4
Sebastian <shuertas981@gmail.com>
2024-04-03 00:12:20 -0500

    Configuracion de Carpetas para el proyecto

---
+++ back/src/main/java/com/cibertec/waifustore/waifustore/WaifuStoreApplication.java
@@ -0,0 +1,13 @@
+package com.cibertec.waifustore.waifustore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaifuStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaifuStoreApplication.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/cibertec/waifustore/waifustore/WaifuStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibertec.waifustore.waifustore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaifuStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
