50b8a67f5b532a236833d720e99e1043f92e3986
NaheliNaupari <137466252+NaheliNaupari@users.noreply.github.com>
2024-04-24 22:30:28 -0500

    Diseño del Footer

--- front/src/app/pages/home/index/components/footer/footer.component.ts
+++ front/src/app/pages/home/index/components/footer/footer.component.ts
@@ -8,5 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
+  empresaTitle = "Nuestra Empresa";
+  empresaContent = "¿Quiénes somos?";
+  terminos = "Términos y Condiciones";
 
+  contactoTitle = "Contáctanos:";
+  whatsappSrc = "/assets/img/whatsapp.png";
+  whatsappAlt = "WhatsApp";
+
+  seguirTitle = "Síguenos:";
+  facebookSrc = "https://makbadxzoirjxbltiuwi.supabase.co/storage/v1/object/public/images/Facebook-icon.svg";
+  facebookAlt = "Facebook";
+
+  pagoTitle = "Medio de Pago:";
+  visaSrc = "/assets/img/visa.png";
+  visaAlt = "Visa";
+  yapeSrc = "/assets/img/yape_1.png";
+  yapeAlt = "Yape";
+  plinSrc = "/assets/img/plin.png";
+  plinAlt = "Plin";
+
+  derechos = "Todos los derechos reservados 2024";
 }
